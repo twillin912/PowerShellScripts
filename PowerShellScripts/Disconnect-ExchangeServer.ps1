6d99ed3088d1f88d20263e7aac86873e86625e40
Trent Willingham <Trent.Willingham@cambiumlearning.com>
2016-10-06 09:42:12 -0400

    Added Disconnect-ExchangeServer function

---
+++ PowerShellScripts/Disconnect-ExchangeServer.ps1
@@ -0,0 +1,39 @@
+function Disconnect-ExchangeServer {
+    <#
+    .SYNOPSIS
+        Use the Disconnect-ExchangeServer cmdlet to disconnect from the Exchange PowerShell web service.
+    .PARAMETER SessionName
+        Optional. The name of the powershell session connected to the Exchange web service.  By default the session name will be 'RemoteExchange'
+    .EXAMPLE
+        This example disconnects from the default PowerShell web service session.
+
+        Disconnect-ExchangeServer
+    .LINK
+        https://github.com/twillin912/PowerShellScripts
+    .NOTES
+        Author: Trent Willingham
+        Check out my other scripts and projects @ https://github.com/twillin912
+
+        Change Log
+        v1.00   2016-10-06   Initial Release
+    #>
+
+    #requires -version 2
+    [CmdletBinding()]
+    Param (
+        [Alias('Identity')]
+        [string]$SessionName = 'RemoteExchange'
+    )
+
+    Process {
+        Try {
+            Get-PSSession -Name $SessionName -ErrorAction Stop
+        }
+        Catch {
+            Write-Warning -Message "Session '$SessionName' not found."
+            Exit
+        }
+        Remove-PSSession -Name $SessionName
+    }
+
+}
